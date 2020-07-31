722bbda188ce382b5a62dc4b237bdb69b1584e66
wang_chang <wang_chang@hoperun.com>
2020-07-31 17:41:34 +0800

    增加ng-packgar

---
+++ dgyx-senne/src/app/framework/rw-p/rw-p-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rw-p-panel',
+  templateUrl: './rw-p-panel.component.html',
+  styleUrls: ['./rw-p-panel.component.scss']
+})
+export class RwPanelComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
---
+++ dgyx-senne/src/app/framework/rw-primeng.module.ts
@@ -0,0 +1,17 @@
+// 引入primeNg的所有模块
+import { NgModule } from '@angular/core';
+import { PrimeNgModule } from './primeng.module';
+
+@NgModule({
+  imports: [
+    PrimeNgModule
+  ],
+  declarations: [
+
+  ],
+  exports: [
+
+  ]
+})
+
+export class RwPrimeNgModule { }
